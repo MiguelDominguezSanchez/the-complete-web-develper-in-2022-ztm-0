a670324592ce2964689b63a5716bf9211fc4ddac
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-19 09:21:09 +0200

    11. Javascript - 011 Functions

--- css-section/06/js/script.js
+++ css-section/06/js/script.js
@@ -1,6 +1,77 @@
+/*
 4 + 3
 if (4 + 3 === 7) {
 	// alert('You are smart!')
 	console.log('Hello')
 }
 console.log('Hello')
+*/
+/*
+function sayHello() {
+	console.log('Hello')
+}
+
+sayHello()
+
+var sayBye = function () {
+	console.log('Bye')
+}
+
+sayBye()
+*/
+
+/*
+function sing() {
+	console.log('Ahhh')
+	console.log('Teee')
+}
+
+sing()
+
+function sing2() {
+	console.log('laaa deee daaa')
+	console.log('laaa laaa')
+}
+
+sing2()
+*/
+
+/*
+function sing(song) {
+	console.log(song)
+}
+
+sing('Laa laaa laaa laa, la la la')
+sing('Hellooo')
+sing('backstreets back all right')
+*/
+
+/*
+function multiply(a, b) {
+	// console.log(a, b)
+	if (a > 10 || b > 10) {
+		return "that's too hard"
+	} else {
+		return a * b
+	}
+}
+*/
+
+/*
+var a = function (a, b) {
+	// console.log(a, b)
+	if (a > 10 || b > 10) {
+		return "that's too hard"
+	} else {
+		return a * b
+	}
+}
+
+// multiply(5, 10)
+a()
+*/
+
+function multiply(a, b) {
+	return a * b
+}
+alert(multiply(3, 4))
