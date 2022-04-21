4c761bd343bc730238c115a3fa24703df1c5fa55
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-21 11:14:29 +0200

    12. DOM Manipulation - 004 DOM Events

---
+++ css-section/07/js/script_.js
@@ -0,0 +1,28 @@
+// document.getElementsByTagName('h1')
+// document.getElementsByClassName("second")
+// document.getElementById("first")
+// ​document.getElementsByClassName("second")[0]​
+// document.querySelector("li")​
+// document.querySelectorAll("li")
+// document.querySelectorAll("li, h1")
+// document.querySelector('li').getAttribute('random')
+// document.querySelector('li').setAttribute('random', '1000')
+// document.querySelector('li')
+// document.querySelector('h1')​
+// ​document.querySelector('h1').style
+// document.querySelector('h1').style.background = "yellow"
+// var h1 = document.querySelector('h1')
+// h1
+// h1.className = 'coolTitle'
+// document.querySelector('li').classList
+// document.querySelector('li').classList.add('coolTitle')
+// document.querySelector('li').classList.remove('coolTitle')
+// document.querySelector('li').classList.add('done')
+// document.querySelector('li').classList.toggle('done')
+// document.querySelector('h1')
+// document.querySelector('h1').innerHTML = '<strong>!!!</strong>'
+// document.querySelector('h1')
+// document.querySelectorAll('li')[1]
+// document.querySelectorAll('li')[1].parentElement
+// document.querySelectorAll('li')[1].parentElement.parentElement
+document.querySelectorAll('li')[1].parentElement.parentElement.children
