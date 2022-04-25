fc4e9668fddfd0e838f6c21d1ad666a54e208cd9
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-25 11:53:46 +0200

    13. Advanced Javascript - 016 ES7 (ES2016)

---
+++ 01-section/13_Advanced_Javascript/09/js/script.js
@@ -0,0 +1,18 @@
+// 'Hellooo'./includes('o')
+/**
+ * 
+
+const pets = ['cat', 'dog', 'bat']
+
+console.log(pets.includes('dog'))
+console.log(pets.includes('bird'))
+ */
+
+// Exponential Operator
+const square = (x) => x ** 2
+const cube = (y) => y ** 3
+
+console.log(square(2))
+console.log(square(5))
+console.log(cube(3))
+console.log(cube(4))
