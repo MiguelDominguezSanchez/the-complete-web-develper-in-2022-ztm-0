7f9b43887c7998ff1e36e6f05d735416afe3092f
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-27 19:17:44 +0200

    13. Advanced Javascript - 001 Scope

--- 01-section/13_Advanced_Javascript/01/js/script.js
+++ 01-section/13_Advanced_Javascript/01/js/script.js
@@ -1,121 +1,147 @@
-/*
-Scope
-//
+/** 
+Welcome back,
+in this video,
+we are gonna talk about an important concept in Javascript
+called 'scope'
+SCOPE
 
-An important concept in Javascript,
-called scope.
-Now what is scope,
+Now wat is 'scope'
 scope means variable access
-what variables do I have access to
-when a code is running.
-//
+what variables do I have to when a code is running
 
-So by default in Javascript you are iin the root scope,
-which is the window object.
+So by default in Javascript
+you are in the root scope which is the window object
 
-Let me show you what I am talking about.
+Let me show you what I am talking about:
 
-If in my console I do:
+If in my console I do something like:
 
 function aa() {
-  console.log("test")
+  console.log('test')
 }
 
-Now te function aa()
-is part of the scope.
+and we close up that function.
 
-If I do window here.(in the console)
-and open up window.
+Well now the:
 
-you see the 'aa' function,
-has been added to the window scope,
+'function aa() {console.log('test')}'
+
+is part of the window scope.
+
+If i do window here,
+and open up window:
+
+Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+$: ƒ ( selector, context )
+Constants: {MID: '18e2c4209383d2d425f2bbdc8d7e092b', CHANNEL: 'bf05da39-8513-4ffa-9460-b7545e6d0b5b'}
+GoogleAnalyticsObject: "ga"
+NProgress: {version: '0.2.0', settings: {…}, status: null, configure: ƒ, set: ƒ, …}
+Popper: ƒ t(o,n)
+Swal: ƒ Ue()
+SweetAlert: ƒ Ue()
+Sweetalert2: ƒ Ue()
+Swiper: ƒ t()
+aa: ƒ aa() ------------------- HERE ----------------------
+alert: ƒ alert()
+
+You see that the aa() function
+has been added to the window scope
+
 So when you are working in a browser
-this is called the 'root' scope,
-so the 'parent' scope.
+this is called the 'root scope'
+so the 'parent scope'
 
-Now what if I do something like this:
+Now, What if I do something like this?
+If I go:
 
-unction bb(){
-  var a = "hello"
+function bb() {
+  // and within this function
+  // I do:
+  var a = "hello";
 }
 
-This function,
-if I run this in the console.
+Well this function,
+if I run this in the console,
 If I now do:
-'console.log(a)'
-will that work?
 
-No because 'a' only lives within the function 'bb'.
+console.log(a)
 
-The scope is inside of the function,
-the only way we can do:
+Will that work?
+
+VM1017:1 Uncaught ReferenceError: a is not defined
+    at <anonymous>:1:13
+
+No, because 'a' only live in the function bb()
+the scope is inside of the function
+
+So the only way we can do 
 'console.log(a)'
-is within the function to add
-'console.log(a)'
+it's within the function
+to add
 
 function bb() {
   var a = "hello";
   console.log(a)
 }
 
-So now if I run the function:
+Now if I run the function
 
-'bb()'
+bb();
 
 I have:
 
-'hello'
+hello
 
- */
+The interesting thing
+is that functions have access to any variable in the root scope
 
-/*
-Now the interesting thing is that
-functions have access to any variable 
-in the 'root' scope.
-
-Now if I do:
+For example if I do:
 
 var b = "Can I access this?"
-
-'function bb()' has access to this variable 'b'
 
 function bb() {
   var a = "hello";
 }
 
-Because as you know,
+function bb()
+has access to this
+because as you know 
+'window.bb() '
+exist
+and variable b
+'var b'
+lives on the window object 
+'window.b'
 
-'window.bb'
+So they both have te same parent
 
-exists and 
+I show you what I mean by copying this
+and copying this into the console
 
-'var b = "Can I access this?"'
-
-lives on the window object.
-So they both have the same parent.
-
-Let me show you what I mean by copying this 
-and put it into the console.
-
-now within 'function bb()' 
-If I do: 
-console.log(b)
-
-var b = "Can I access this?"
+var b = "Can I access this?";
 
 function bb() {
-  var a = "hello"
+  var a = "hello";
+}
+
+now in: 
+
+var b = "Can I access this?";
+
+function bb() {
+  var a = "hello";
+  // if we do
   console.log(b)
 }
 
-We have access to this:
+Well we have access this:
 
-'Can I access this?'
+bb()
+Can I access this?
 
-*/
-
-/*
-Ok what if I do something along the lines of:
+ok what if I do something about the lines 
+of variable b can I access this?
+I do b equals hello
 
 var b = "Can I access this?";
 
@@ -123,55 +149,65 @@ function bb() {
   b = "hello";
 }
 
-and now I do:
+if I copy this
+and now I do: 
 
-'console.log(b)'
+console.log(b)
 
 I have:
 
-'Can I access this?'
+console.log(b)
+Can I access this?
 
-Because I haven't run the function.
-But If I run the function 
-
+because I haven't run the function
+but if I run the function 
 'bb()'
-
-Now if I do console log variable 'b':  
-
-'console.log(b)'
+Now if I do 
+console.log()
+variable b
+console.log(b)
 
 I get:
 
-'hello'
+console.log(b)
+hello
 
-Because 'b' is the same variable.
-*/
+Because 'b' is the same variable
 
-/*
+//
 
-ok lets do something a little bit more complex.
-So knowing what we know,
-We know that this is the root scope
+Ok lets do something a little bit more complex,
+so knowing what we know
+that this is the root scope
 in our case is the window
 
-Root Scope (window)
+// Root Scope (window)
+Within here we can declare a variable
+var fun = 5
+and we also declare a function
+funFunction()
 
-within here we can declare a variable
+function funFunction() {
+// and within the function 
+// as soon as we create tese curly brackets
+// We have created a child scope within here
+//
+// child scope
+//
+var fun = "hellooo";
+console.log(fun)
+}
 
-'var fun = 5'
+//
 
-we also declare a function,
-
-'function funFunction() {
-  // as soon as we create this curly braces we've created a child scope in here
-  // So within here I say
-  var fun = "hellooo"
-  // and console log fun
+function funFunction() {
+  // child scope
+  var fun = "hellooo";
   console.log(fun)
-}'
+}
 
-and we create another function,
-lets say this one we call it funner
+and we create another function
+and this one we will call it funnerFunction
 
 function funnerFunction() {
   // child scope
@@ -179,209 +215,216 @@ function funnerFunction() {
   console.log(fun)
 }
 
-And finally we create a third function
-and we call this the funnest
+and finally we create a third function
+and we call this the funnestFunction
 
 function funnestFunction() {
   // child scope
-  // and within here we romevo the variable
-  // and we say: 
+  //
+  // and within here we remove the variable
+  // and we say AHHH
   fun = "AHHH"
   console.log(fun)
 }
 
-and at the bottom we say console log fun
-
+and at the bottom we 
 console.log(fun)
 
-Let's go over this
-We declared a variable fun equals five
-We declared three functions.
-And each of these functions,
-create their own scope,
-and each of these functions,
-console log fun,
-but what fun means,
-in each one of them,
-is different based on their scope.
+So let's go over this
+we declare a variable fun
 
-So the first one
-we title it '1',
-just so we know,
-the second,
-lets say '2',
-the third '3'
-and finally here we just say, 'window'
+We've created tree functions
+and each of these function
+create their own scope
+and each of these function console log 'fun'
 
+But what fun means in each one of them
+is different based on their scope
+so the first one we title it 1,
+so just we know
+the second we say 2
+the third 3
+and then finally here we just say 'window'
+So lets run this function and see what happens
 
-var fun = 5
+var fun = 5;
 
 function funFunction() {
-  var fun = "hellooo"
+  // child scope
+  var fun = "hellooo";
   console.log(1, fun)
 }
 
 function funnerFunction() {
+  // child scope
   var fun = "Byee";
   console.log(2, fun)
 }
 
 function funnestFunction() {
-  fun = "AHHH"
+  // child scope
+  fun = "AHHH";
   console.log(3, fun)
 }
 
-console.log('window', fun)
+console.log("window", fun)
 
-*/
+Lets see if we can guess whats about to happen
 
-/* 
-
-So we have
+so we have:
 
 'window 5'
 
-which means this line over here:
+which means this line over here
+'console.log("window", fun)'
 
-'console.log('window', fun)'
+the fun variable is well 5
 
-the fun variable is '5'
-nothing has been run,
-we read through this script 
-and we see the variable fun equals five
+'var fun = 5'
 
-var fun = 5
-
-and then we see these functions 
-but none of them are run
-
-but now if I do 
-'funFunction'
-and we actually run this
+noting has been run
+we read through the script
+and we see that variable fun equals 5
+and then we see these functions,
+but not of them are run
+and now if I do:
 'funFunction()'
+to actually run this
 and we do 
 'funnerFunction()'
-and finally the 
-'funnestFunction()'
+and finally the funniest function
+'funniestFunction()'
 
-
-var fun = 5
+var fun = 5;
 
 function funFunction() {
-  var fun = "hellooo"
-  console.log(1, fun)
+  // child scope
+  var fun = "hellooo";
+  console.log(1, fun);
 }
 
 function funnerFunction() {
+  // child scope
   var fun = "Byee";
-  console.log(2, fun)
+  console.log(2, fun);
 }
 
 function funnestFunction() {
-  fun = "AHHH"
-  console.log(3, fun)
+  // child scope
+  fun = "AHHH";
+  console.log(3, fun);
 }
 
-console.log('window', fun)
-funFunction()
-funnerFunction()
-funnestFunction()
+console.log("window", fun);
+funFunction();
+funnerFunction();
+funnestFunction();
 
-I copy and paste this the console:
+if I copy and paste this and this
+we see we have:
 
-We see we have:
+window 5 // line got run which is variable 5
+1 hellooo // within the  first function, we have fun equals to hellooo, because we have created a new scope, within this function lives variable fun as 'hellooo'. So that get overwritten
+2 Byee // and then we have the second function that has 'Bye'. Again the same thing as above, a new s, a new variable, fun equals 'Bye'
+3 AHHH // and the third one, we see that we have find equals 'AHHH', and again tat gets changed and that equals to fun
 
-window 5 
-1 hellooo 
-2 Byee 
-3 AHHH
-
-window 5 // line console.log('window', fun)
-I got run which is variable five, var fun = 5;
-1 hellooo // The first function we have the variable fun equals to hello.
-Because we ' ve created a new scope, and within this function lives variable fun. var fun = 'hellooo'
-That has hello, so that gets overwritten.
-2 Byee // And then we have the second function that has bye.
-Again the same thing as above. Because we have a new scope.
-A new variable fun equals bye. var fun = "Byee"
-3 AHHH // And the third one, we have fun equals "AHHH",
-and again that gets changed
-
-*/
-
-/*
-here is the fun part though
-If I do:
-console.log(fun)
-in the browser console
-its now equals to 'AHHH'
-Because in the third function we modified
-fun which is this variable,
-equals to 'AHHH'
-fun = "AHHH"
-
-One thing that I am gonna show you 
-its that in the first two functions
-I wont be able to access the 
-fun variable
-var fun = 5
+Here is the fun part though
+if I now do:
+'console.log(fun)'
+its now equals to:
+'AHHH'
+Because in the third function 
+we have modified 'fun'
+which is this variable
+'var fun = 5'
+to equal to 'AHHH'
+One thing I gonna show you
+is that in the first two functions
+I wont be able to access the fun variable
 in the root scope
 because I essentially overwritten them
-within the funFunction,
-and the funnerFunction,
-fun equals something
-that its their own meaning,
-so within this funFunction
-'funFunction()'
-fun equals hellooo
-within the funnerFunction
-'funnerFunction()'
-fun equals bye
-and no matter what 
-we wont have access to
-the root scope.
-And this is called a:
-'Naming Conflict',
-where we name things the same way
-as something that lives let's say 
-in the parent scope
-and we lose access to it.
+because the funFunction and the funnerFunction
+fun equals something that is their own meaning
+so this function 'funFunction()' fun equals 'hello'
+within the 'funnestFunction()' fun equals 'Byee'
+and no matter what
+we wont have access to the root scope
+'var fun = 5'
+it is called a naming conflict
+when we name things the same way
+that lives lets say in the parent scope
+and we lose access to it
 
-So to finalize this point
+So to finalize this this point
+let just delete this and go over
+whats happening here
+so lets delete this,
+and just a console.log here
 
-when somebody runs
+
+
+var fun = 5;
 
 function funnestFunction() {
+  // child scope
   console.log(fun)
+  // we look at the scope here
+  // hey do you know what fun is
+  // and the function say
+  // sorry friend I dont really know fun
+  // ask my parent
 }
 
-we run this and javascript reads 
-console.log(fun)
-ok, fun, do we know her?
-we look at the scope here in the function
-Hey, do you know what fun is?
-and the function says, no, sorry friend
-I dont really know fun
-but ask my parent,
-so now we go outside of the function,
+so now we go outside the function
 in this case the root scope
-and we say,
-hey, do you know fun?
-Yes, we have a variable fun equals five,
-so now console log logs five.
+Hey, do you know fun?
+ye ye ye, we have a variable fun equals 5
+so now console.log can log 5
+if if fun doesn't exist ...
+we ask te parent
+and the parent says 
+sorry, I have no idea what fun is
+then we get an error
+so the last check is our root scope
+in our case the window object
+if we cant find it we get this error in the console
+sorry your friend variable doesn't exist
+I think you made it up
 
-But if fun doesn't exists,
-we ask the parent and the parent
-says, sorry I have no idea what fun is,
-and we get an error,
-so the last check is always a root scope,
-in our case the window object,
-if we cant find it
-we get this error in the console we always see
-your friend variable doesn't exist
-I think you make her up
+and that's it for scope
 
-thats it for scope
+I have a fun exercise for you right after this video
+give it a try
+I see you in the next one
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 */
