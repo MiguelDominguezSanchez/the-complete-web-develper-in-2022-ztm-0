83993624544f6c2a81f78b2dccd2023bee476055
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-28 10:10:12 +0200

    13. Advanced Javascript - Ternary Operator

--- 01-section/13_Advanced_Javascript/02/js/script.js
+++ 01-section/13_Advanced_Javascript/02/js/script.js
@@ -86,7 +86,151 @@ var automatedAnswer =
 "Your account # is "= ( 
   // Because we have this braces
   // we can have an expression inside of here
-  isUserValid(false) ? "1234" : "not available" )
+  // that we can evaluate
+  isUserValid(false) 
+  // if the user is valid
+  ?
+  // we say 
+  "1234"
+  // if not
+  : 
+  // we say
+  "not available" 
+  )
+
+
+if we copy  and paste this in our console
+and we can see it better
+if user is valid we can say "1234" iif not "not available"
+
+// 
+
+function isUserValid(bool) {
+  return bool;
+}
+
+var answer = isUserValid(true) ? "You may enter" : "Access Denied"
+
+var automatedAnswer =
+"Your account # is " + ( isUserValid(false) ? "1234" : "not available")
+
+// 
+
+and now I do: 
+'automatedAnswer'
+I get:
+"Your account # is not available"
+
+Well because 
+'isUserValid(false) '
+is false
+but if I give it 'true'
+isUserValid(true)
+
+// 
+
+function isUserValid(bool) {
+  return bool;
+}
+
+var answer = isUserValid(true) ? "You may enter" : "Access Denied"
+
+var automatedAnswer =
+"Your account # is " + ( isUserValid(true) ? "1234" : "not available")
+
+// 
+
+well in that case:
+'automatedAnswer'
+"Your account # is 1234"
+
+You might notice that 
+this is actually not that different from if we do:
+
+// 
+
+function condition() {
+  if(isUserValid(true)) {
+    return "You may enter";
+  } else {
+    return "Access denied"
+  }
+}
+
+//
+
+This function over here condition
+if we do: 
+
+var answer2 = condition()
+
+and copy these two:
+and run them:
+
+function isUserValid(bool) {
+  return bool;
+}
+
+var answer = isUserValid(true) ? "You may enter" : "Access Denied"
+
+function condition() {
+  if (isUserValid(true)) {
+    return "You may enter";
+  } else {
+    return "Access denied"
+  }
+}
+
+var answer2 = condition();
+
+// 
+
+and now if I look at answer
+
+answer
+"You may enter"
+
+if I do answer2
+I get
+
+answer2
+"You may enter"
+
+Now its the exact same thing
+but you see one way is nicer,
+a simple one line:
+
+var answer = isUserValid(true) ? "You amy enter" : "Access Denied"
+
+a ternary operator is really really good for this
+if else
+where there is a check for condition
+and there are two possible expression
+that come out of it
+
+*/
+
+//
+
+/*
+ok lets learnt the next one which iis the switch statement
+this is a fun one
+
+So a switch statement looks something like this
+you can build a simple game right now
+to demonstrate how it works
+
+I can say function moveCommand
+
+function moveCommand(
+  // and we will say:
+  // it accepts parameter 'direction'
+)
+
+
+
+
+
 
 
 
