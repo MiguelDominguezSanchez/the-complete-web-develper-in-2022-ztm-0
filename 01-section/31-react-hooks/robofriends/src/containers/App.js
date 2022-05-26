f31cc12247cff7f8db6dbc9bc00393131b8135d3
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-05-26 08:47:24 +0200

    31. React Hooks - 008 React Hooks 8

--- 01-section/31-react-hooks/robofriends/src/containers/App.js
+++ 01-section/31-react-hooks/robofriends/src/containers/App.js
@@ -2,32 +2,21 @@ import React, { useState, useEffect } from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
-import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css'
 
 function App() {
-	// constructor() {
-	// 	super()
-	// 	this.state = {
-	// 		robots: [],
-	// 		searchfield: '',
-	// 	}
-	// }
 	const [robots, setRobots] = useState([])
 	const [searchfield, setSearchfield] = useState('')
+	const [count, setCount] = useState(0)
 
-	// componentDidMount() {
-	// 	fetch('https://jsonplaceholder.typicode.com/users')
-	// 		.then((response) => response.json())
-	// 		.then((users) => this.setState({ robots: users }))
-	// }
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((response) => response.json())
 			.then((users) => {
 				setRobots(users)
 			})
-	}, [])
+		console.log(count)
+	}, [count]) // only run if count changes.
 
 	const onSearchChange = (event) => {
 		setSearchfield(event.target.value)
@@ -42,11 +31,10 @@ function App() {
 	) : (
 		<div className='tc'>
 			<h1 className='f1'>RoboFriends</h1>
+			<button onClick={() => setCount(count + 1)}>Click Me!</button>
 			<SearchBox searchChange={onSearchChange} />
 			<Scroll>
-				<ErrorBoundry>
-					<CardList robots={filteredRobots} />
-				</ErrorBoundry>
+				<CardList robots={filteredRobots} />
 			</Scroll>
 		</div>
 	)
